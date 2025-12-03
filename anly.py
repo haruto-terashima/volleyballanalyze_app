e62f7c1367f38ee4da7269bd0e369ccfed2840a3
haruto-terashima <hal.xyz@icloud.com>
2025-12-04 02:42:00 +0900

    ログイン・グラフ機能など修正

--- anly.py
+++ anly.py
@@ -1,15 +1,39 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, Response
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')  
+import io
+from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+
+# いったんランダム、本番で書き換える
+app.config["SECRET_KEY"] = os.urandom(24)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+dt_info = {
+    'user':'haruto',
+    'password':'Ha-ru-811',
+    'host':'localhost',
+    'database':'webapp_vb'
+}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{user}:{password}@{host}/{database}'.format(**dt_info)
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     day = db.Column(db.DateTime, nullable=False)
-    match_name = db.Column(db.String(500), nullable=False)
+    match_name = db.Column(db.String(100), nullable=False)
     reflect = db.Column(db.String(500))
     all_attack = db.Column(db.Integer, nullable=False)
     score_attack = db.Column(db.Integer, nullable=False)
@@ -19,7 +43,54 @@ class Post(db.Model):
     score_serve = db.Column(db.Integer, nullable=False)
     miss_serve = db.Column(db.Integer, nullable=False)
 
-@app.route('/', methods=['GET', 'POST'])
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50),nullable=False, unique=True)
+    password = db.Column(db.String(200),nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/signup', methods = ['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        hashed_pass = generate_password_hash(password)
+        user = User(username = username, password = hashed_pass)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('signup.html')
+    
+@app.route('/', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username = username).first()
+        if check_password_hash(user.password, password=password):
+            login_user(user)
+            return redirect('/index')
+        else:
+            return redirect('/', msg = 'ユーザー名かパスワードが違います')
+    else:
+        return render_template('login.html', msg = '')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+
+
+
+
+@app.route('/index', methods=['GET', 'POST'])
+@login_required
 def index():
     if request.method == 'GET':
         posts = Post.query.order_by(Post.day).all()
@@ -53,28 +124,32 @@ def index():
         
         db.session.add(new_post)
         db.session.commit()
-        return redirect('/')
+        return redirect('/index')
 
 
 @app.route('/write')
+@login_required
 def write():
     return render_template('write.html')
 
 @app.route('/reflect/<int:id>')
+@login_required
 def reflect(id):
     post = Post.query.get(id)
 
     return render_template('reflect.html', post=post)
 
 @app.route('/delete/<int:id>')
+@login_required
 def delete(id):
     post = Post.query.get(id)
 
     db.session.delete(post)
     db.session.commit()
-    return redirect('/')
+    return redirect('/index')
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
+@login_required
 def update(id):
     post = Post.query.get(id)
     if request.method == 'GET':
@@ -92,9 +167,10 @@ def update(id):
         post.miss_serve = request.form.get('miss_serve')
 
         db.session.commit()
-        return redirect('/')
+        return redirect('/index')
     
 @app.route('/analyze/<int:id>')
+@login_required
 def analyze(id):
     post = Post.query.get(id)
     if post.all_attack > 0:
@@ -122,7 +198,153 @@ def analyze(id):
                            koritu_serve=koritu_serve,
                            fail_serve=fail_serve 
                            ) 
-                           
+
+@app.route('/plot_attack.png')
+@login_required
+def plot_attack_png():
+    posts = Post.query.order_by(Post.day).all()
+
+    labels = []
+    seiko_attack = []
+    koritu_attack = []
+    blocked_attack = []
+    fail_attack = []
+
+    for p in posts:
+        
+        date_str = p.day.strftime('%Y-%m-%d')
+        labels.append(date_str)
+
+        if p.all_attack > 0:
+            rate = (p.score_attack / p.all_attack) * 100
+            rate = round(rate, 1)
+        else:
+            rate = 0
+        
+        seiko_attack.append(rate)
+
+        if p.all_attack > 0:
+            rate = ((p.score_attack - p.miss_attack) / p.all_attack) * 100
+            rate = round(rate, 1)
+        else:
+            rate = 0
+        koritu_attack.append(rate)
+
+        if p.all_attack > 0:
+            rate = (p.block_attack / p.all_attack) * 100
+            rate = round(rate, 1)
+        else:
+            rate = 0
+        blocked_attack.append(rate)
+
+        if p.all_attack > 0:
+            rate = (p.miss_attack / p.all_attack) * 100
+            rate = round(rate, 1)
+        else:
+            rate = 0
+        fail_attack.append(rate)
+
+    fig, ax1 = plt.subplots(figsize=(10, 4))
+
+
+    ax1.plot(labels, seiko_attack, marker='o', label='攻撃成功率')
+    ax1.set_title("攻撃関連の推移 (%)")
+    ax1.set_ylabel("成功率 (%)")
+    ax1.set_xlabel("試合日")
+    ax1.legend()
+
+
+    ax1.plot(labels, koritu_attack, marker='x', label = '攻撃効果率')
+    ax1.legend()
+
+    ax1.plot(labels, blocked_attack, marker='^', label='ブロックされた割合')
+    ax1.legend()
+
+
+    ax1.plot(labels, fail_attack, marker='s', label='攻撃失敗率')
+    ax1.legend()
+
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    img = io.BytesIO()
+    fig.savefig(img, format='png')
+    plt.close(fig)
+    img.seek(0)
+
+    return Response(img.getvalue(), mimetype='image/png')
+
+@app.route('/plot_serve.png')
+@login_required
+def plot_serve_png():
+    posts = Post.query.order_by(Post.day).all()
+    
+    labels = []
+    seiko_serve = []
+    koritu_serve = []
+    fail_serve = []
+    
+    for p in posts:
+
+        date_str = p.day.strftime('%Y-%m-%d')
+        labels.append(date_str)
+
+        if p.all_serve > 0:
+            rate = (p.score_serve / p.all_serve) * 100
+            rate = round(rate, 1)
+        else:
+            rate = 0
+        seiko_serve.append(rate)
+
+        if p.all_serve > 0:
+            rate = ((p.score_serve - p.miss_serve) / p.all_serve) * 100
+            rate = round(rate, 1)
+        else:
+            rate = 0
+        koritu_serve.append(rate)
+
+   
+        if p.all_serve > 0:
+            rate = (p.miss_serve / p.all_serve) * 100
+            rate = round(rate, 1)
+        else:
+            rate = 0
+        fail_serve.append(rate)
+
+    fig2, ax2 = plt.subplots(figsize=(10, 4))
+
+
+    ax2.plot(labels, seiko_serve, marker='o', label='サーブ成功率')
+    ax2.set_title("サーブ関連の推移 (%)")
+    ax2.set_ylabel("成功率 (%)")
+    ax2.set_xlabel("試合日")
+    ax2.legend()
+
+
+    ax2.plot(labels, koritu_serve, marker='x', label='サーブ効果率')
+    ax2.legend()
+
+
+    ax2.plot(labels, fail_serve, marker='s', label='サーブ失敗率')
+    ax2.legend()
+
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    img2 = io.BytesIO()
+    fig2.savefig(img2, format='png')
+    plt.close(fig2)
+    img2.seek(0)
+
+    return Response(img2.getvalue(), mimetype='image/png')
+
+
+
+@app.route('/graph')
+@login_required
+def graph():
+    return render_template('graph.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
